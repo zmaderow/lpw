e754a4c7960992b3904e61fa0bbd414ba0797028
zmaderow <88632400+zmaderow@users.noreply.github.com>
2023-06-20 19:25:16 -0300

    Add files via upload

--- scripts/functions.js
+++ scripts/functions.js
@@ -3,9 +3,8 @@
 let lightMode = true
 
 function turnMode() {
-    document.querySelector('body').style.backgroundColor = lightMode ? 'black' : 'white';
-    document.querySelector('body').style.color = lightMode ? 'white' : 'black';
-    document.querySelector('.box').style.color = lightMode ? 'white' : 'black';
-    document.querySelector('.mid .box').style.borderColor = lightMode ? 'white' : 'black';
+    document.documentElement.style.setProperty('--body-bg', lightMode ? 'black' : 'white');
+    document.documentElement.style.setProperty('--body-color', lightMode ? 'white' : 'black');
+    document.documentElement.style.setProperty('--image-invert', lightMode ? 1 : 0);
     lightMode = !lightMode
 }
